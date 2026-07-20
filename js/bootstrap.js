773c110ebe1df10afb3481fd93dc33986b52ec15
fearonc <connor_fearon@hotmail.co.uk>
2026-07-20 11:31:24 +0100

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -25,6 +25,7 @@ const files = [
   "tools/dark-overlay.js",
   "tools/pink-overlay.js",
   "tools/bulk-update.js",
+  "tools/bulk-linked-options.js",
   "tools/image-tools.js",
   "tools/audit-search.js",
   "tools/quote-wrap.js",
